c7477e468ba756eb4b9c6d33c64a9381bec777c4
Patrick Bauer <patrick.bauer91@gmail.com>
2023-03-11 18:05:57 +0100

    remove boilerplate files

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './styles/main.scss'
+
 
 const app = createApp(App)
 
