871c9ad8e73f8dba5e4c4559152a242f13e271be
Patrick Bauer <patrick.bauer91@gmail.com>
2023-03-14 17:34:40 +0100

    add live block component

---
+++ src/components/LiveBlock.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="font-mono n-live-block relative break-all uppercase">
+    <span class="absolute n-block">67041a3309268109175310b763f3c09f8802c572b4807a8b2675872d630facde</span>
+    <span class="absolute opacity-40 text-grey-500 n-block">67041a3309268109175310b763f3c09f8802c572b4807a8b2675872d630facde</span>
+    <span class="absolute opacity-20 text-grey-500 n-block">67041a3309268109175310b763f3c09f8802c572b4807a8b2675872d630facde</span>
+    <span class="absolute opacity-10 text-grey-500 n-block">67041a3309268109175310b763f3c09f8802c572b4807a8b2675872d630facde</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.n-live-block {
+  font-size: 42px;
+  line-height: 44px;
+  width: 280px;
+  height: 280px;
+}
+
+.n-block {
+  transform-origin: center;
+
+  &:nth-child(2) {
+    transform: scale(.97);
+  }
+
+  &:nth-child(3) {
+    transform: scale(.95);
+  }
+
+  &:nth-child(4) {
+    transform: scale(.93);
+    color: #E0DAEE
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import LiveBlock from '@/components/LiveBlock.vue'
 </script>
 
 <template>
   <main>
-    <div class="container mx-auto" />
+    <LiveBlock />
   </main>
 </template>
+
