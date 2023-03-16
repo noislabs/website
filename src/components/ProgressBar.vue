504111781ed7f8d98f14e2680aa8a7c12ada4e45
Patrick Bauer <patrick.bauer91@gmail.com>
2023-03-16 12:18:58 +0100

    add progress bar animation

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -7,13 +7,23 @@ withDefaults(defineProps<{
 </script>
 
 <template>
-  <div class="h-0.5 w-full bg-grey-800">
-    <div
-      class="h-0.5 bg-grey-50"
-      :style="`width: ${value}%`"
-    />
-  </div>
+  <div class="w-full n-progress h-0.5" />
 </template>
 
 <style lang="scss" scoped>
+.n-progress {
+  background-image: repeating-linear-gradient(to left, theme('colors.grey.800') 0%, theme('colors.grey.800') 50%, theme('colors.grey.50') 50%, theme('colors.grey.50') 100%);
+  background-size: 200% auto;
+  background-position: 0 100%;
+  animation: progressAnim linear 30s infinite;
+}
+
+@keyframes progressAnim {
+  0% {
+    background-position: 100% 50%
+  }
+  100% {
+    background-position: 0 50%
+  }
+}
 </style>
