b0fb0474182631ae9ba33e4b7204ddbf850dee49
Patrick Bauer <patrick.bauer91@gmail.com>
2023-03-21 14:21:31 +0100

    design DAO logo grayscale filter

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <footer class="flex max-sm:flex-col items-center sm:justify-between text-xs py-6 pt-9">
-    <div class="font-medium text-grey-600">
+  <footer class="flex max-sm:flex-col text-grey-600 items-center sm:justify-between text-xs py-6 pt-9">
+    <div class="font-medium">
       <a
         href="https://nois.network"
         class="font-light mr-8 hover:text-grey-100 transition-colors"
@@ -24,17 +24,27 @@
     <a
       href="https://designdao.io/"
       target="_blank"
-      class="flex items-center max-sm:mt-6"
+      class="n-design-dao flex items-center max-sm:mt-6"
     >
-      <span class="uppercase text-grey-700 font-light mr-1.5">Website by</span>
+      <span class="uppercase font-light mr-1.5">Website by</span>
       <img
         src="@/assets/design-dao/designdao.png"
         alt="Design DAO"
         width="25"
         height="19"
-        class="inline-block"
+        class="inline-block grayscale  transition"
       >
-      <span class="uppercase text-grey-50 ml-2.5">Design DAO</span>
+      <span class="uppercase ml-2.5">Design DAO</span>
     </a>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+.n-design-dao {
+  &:hover {
+    img {
+      @apply grayscale-0;
+    }
+  }
+}
+</style>
